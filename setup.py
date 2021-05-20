af4e4249127d8feb8d7ee79dfdb6b560ac5d5e98
TheKingOfTime <artem.matvienko0@gmail.com>
2021-05-20 11:30:42 +0300

    Add: setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sdc_api",
+    version="1.0.1",
+    author="TheKingOfTime",
+    author_email="artem.matvienko0@gmai.com",
+    description="An async wrapper for Server-Discord.Com API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheKing-OfTime/sdc-api.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
