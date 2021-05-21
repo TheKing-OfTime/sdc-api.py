840970f3848ae65fb08bcfc84891774391c1dfa5
SadBoy228 <k0tletka@thunix.net>
2021-05-21 12:54:56 +0300

    Examples: Update monitoring example

--- examples/Monitoring_e.py
+++ examples/Monitoring_e.py
@@ -1,11 +1,24 @@
 import asyncio
-from classes.Monitoring import Monitoring
+import sdc_api_py
 
-Sdc = Monitoring("")
+SDC_TOKEN = "PLACE YOUR TOKEN HERE"
+GUILD_ID = 500020124515041283
+USER_ID = 329688477073539072
+
+monitoring = sdc_api_py.Monitoring(SDC_TOKEN)
 
 
 async def test():
-    response = await Sdc.get_user_rate(500020124515041283)
-    print(response.plus_count, response.minus_count)
+    sdc_guild = await monitoring.get_guild(GUILD_ID)
+    print(f"Name of guild: {sdc_guild.name}; Members amount: {sdc_guild.members}")
+
+    sdc_guild_place = await monitoring.get_guild_place(GUILD_ID)
+    print(f"Place of guild in server list: {sdc_guild_place.place}")
+
+    sdc_guild_rate = await monitoring.get_guild_rate(GUILD_ID)
+    print(f"Rating of guild on monitoring: {sdc_guild_rate.plus - sdc_guild_rate.minus}")
+
+    sdc_user_rate = await monitoring.get_user_rate(GUILD_ID)
+    print(f"Positive rates of user: {sdc_user_rate.plus_count}")
 
 asyncio.run(test())
